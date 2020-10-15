d9372840ca1b3426c3c47dbc5e682e13f7b37321
jatinbharadwaj <jitinrcks@gmail.com>
2020-10-16 00:19:56 +0530

    first commit

---
+++ babble_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for babble_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'babble_django.settings')
+
+application = get_wsgi_application()
