8d9a3c6455e82187de8e3bf7214bd78c39ae90b4
jatinbharadwaj <jitinrcks@gmail.com>
2020-10-20 00:36:38 +0530

    Successful form redirect And Safe

--- babble_django/settings.py
+++ babble_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8kisrj#jrsu$^@d-9h4g8%hv6v)x5g1ew$%yti8nt^q(iuw-tm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
