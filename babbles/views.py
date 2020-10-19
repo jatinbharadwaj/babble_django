85732c4727631c29fb4d26382a322c9c5afec117
jatinbharadwaj <jitinrcks@gmail.com>
2020-10-20 00:37:09 +0530

    safe redirect

--- babbles/views.py
+++ babbles/views.py
@@ -1,16 +1,21 @@
 from django.http import HttpResponse,JsonResponse,Http404
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.utils.http import is_safe_url
+from django.conf import settings
 from .models import Tweet
 from .forms import TweetForm
 # Create your views here.
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 def tweet_create_view(request,*args,**kwargs):
     form = TweetForm(request.POST or None)
-    print('post data is',request.POST)
+    next_url = request.POST.get("next") or None
+    print('next_url',next_url)
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url != None and is_safe_url(next_url,ALLOWED_HOSTS):
+            return redirect(next_url)
         form = TweetForm()
     return render(request,'Components/form.html',context={"form":form})
 
