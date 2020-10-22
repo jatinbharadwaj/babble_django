1c963aeeafc5cc4de09d4f0c37227f6d07ac6df7
jatinbharadwaj <jitinrcks@gmail.com>
2020-10-22 23:30:06 +0530

    search tweet id

--- babble-react/src/tweets/components.js
+++ babble-react/src/tweets/components.js
@@ -1,5 +1,6 @@
-import React, {useState}  from 'react'
-
+import React, {useEffect, useState}  from 'react'
+import {Tweet} from './detail'
+import {apiTweetDetail} from './lookup'
 import {TweetCreate} from './create'
 import {TweetsList} from './list'
 
@@ -18,4 +19,27 @@ export function TweetsComponent(props) {
           <TweetsList newTweets={newTweets} {...props} />
           </div>
   }
-           
+  
+  export function TweetDetailComponent(props){
+    const {tweetId} = props
+    const [didLookup, setDidLookup] = useState(false)
+    const [tweet, setTweet] = useState(null)
+  
+    const handleBackendLookup = (response, status) => {
+      if (status === 200) {
+        setTweet(response)
+      } else {
+        alert("There was an error finding your tweet.")
+      }
+    }
+    useEffect(()=>{
+      if (didLookup === false){
+  
+        apiTweetDetail(tweetId, handleBackendLookup)
+        setDidLookup(true)
+      }
+    }, [tweetId, didLookup, setDidLookup])
+  
+    return tweet === null ? null : <Tweet tweet={tweet} className={props.className} />
+   } 
+   
