f8747e5ae792a691aa9cf7a92502eb3cbd49eccf
jatinbharadwaj <jitinrcks@gmail.com>
2020-10-22 22:12:30 +0530

    Action Button

--- babble-react/src/tweets/lookup.js
+++ babble-react/src/tweets/lookup.js
@@ -5,6 +5,10 @@ export function apiTweetCreate(newTweet, callback){
     backendLookup("POST", "/babbles/create", callback, {content: newTweet})
   }
   
+  export function apiTweetAction(tweetId, action, callback){
+    const data = {id: tweetId, action: action}
+    backendLookup("POST", "/babbles/action", callback, data)
+  }
 export function apiTweetList(callback) {
     backendLookup("GET", "/babbles/", callback)
 }
