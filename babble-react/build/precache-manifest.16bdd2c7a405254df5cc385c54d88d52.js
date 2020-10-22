e703f6196c75f9fe0c9b9d121cd5b5ae857530fc
jatinbharadwaj <jitinrcks@gmail.com>
2020-10-22 17:13:33 +0530

    kuch bhi

---
+++ babble-react/build/precache-manifest.16bdd2c7a405254df5cc385c54d88d52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724724b801ad786f127af848bfbf8e5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e158cb2d2c1cb1e53ef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86ead4cbe9ab4ea04bb3",
+    "url": "/static/js/2.df06a795.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.df06a795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e158cb2d2c1cb1e53ef",
+    "url": "/static/js/main.a907db82.chunk.js"
+  },
+  {
+    "revision": "62dca91f7ad6b05af4cb",
+    "url": "/static/js/runtime-main.75613886.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
