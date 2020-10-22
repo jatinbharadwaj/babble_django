36d726bd5b6659558554440d78df79d3d1d4d5f1
jatinbharadwaj <jitinrcks@gmail.com>
2020-10-22 05:40:48 +0530

    Better Lookup

--- babble-react/src/lookup/components.js
+++ babble-react/src/lookup/components.js
@@ -1,18 +1,21 @@
-export function loadTweets(callback){
-    const xhr = new XMLHttpRequest() 
-    const method ='GET'
-    const url ='http://localhost:8000/api/babbles/'
-    const responseType ='json'
-    xhr.responseType = responseType
-    xhr.open(method,url)
-    // xhr.setRequestHeader("HTTP_X_REQUESTED_WITH","XMLHttpRequest")
-    // xhr.setRequestHeader("X-REQUESTED-WITH","XMLHttpRequest")
-    xhr.onload = function(){
-      callback(xhr.response,xhr.status)
-    }
-    xhr.onerror = (e)=>{
-      console.log(e)
-      callback({"message":"Error Request"},400)
-    }
-  xhr.send()
+function lookup(method, endpoint, callback, data) {
+  let jsonData;
+  if (data){
+    jsonData = JSON.stringify(data)
   }
+  const xhr = new XMLHttpRequest()
+  const url = `http://localhost:8000/api${endpoint}`
+  xhr.responseType = "json"
+  xhr.open(method, url)
+  xhr.onload = function() {
+    callback(xhr.response, xhr.status)
+  }
+  xhr.onerror = function (e) {
+    console.log(e)
+    callback({"message": "The request was an error"}, 400)
+  }
+  xhr.send(jsonData)
+}
+export function loadTweets(callback) {
+    lookup("GET", "/babbles/", callback)
+}
